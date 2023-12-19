438676d0d90c5dfc7eded1c95a5e3c0c041c81ef
m842022 <madikerimbaev7@gmail.com>
2023-12-20 01:06:44 +0600

    Добавлен скрипт и созданы переменные

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Создаем переменные 
+
+var title = "Название сайта";
+var screens = 3;
+var screenPrice = 100;
+var roolback = true;
+var fullPrice = screens * screenPrice;
+var adaptive = true;
+
+// Выводим модальное окно (Alert)
+alert("Привет! Glo-academy");
+
+// Вводим сообщение в консоль
+console.log("Это сообщение в консоли. Здесь тоже можно вывести любой текст.");
+
+// Выводим сообщение в консоль
+console.log("Название сайта", title);
+console.log("Количество экранов:", screens);
+console.log("Цена одного экрана:", screenPrice);
+console.log("Имеется ли откат:", roolback);
+console.log("Общая цена:", fullPrice);
+console.log("Адаптивный ли дизайн", adaptive);
